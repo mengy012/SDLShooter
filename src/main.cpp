f22d8d21ad45a938647f29348150030ba5af7257
mengying <nibabatang@gmail.com>
2026-01-22 17:26:42 +0800

    visual_studio

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,15 @@
-#include <iostream>
+﻿#include <iostream>
 #include <SDL.h>
 #include <SDL_image.h>
 #include <SDL_mixer.h>
 #include <SDL_ttf.h>
 #include "Game.h"
 
-int main(int, char **)
+int main(int, char**)
 {
-    Game &game = Game::getInstance();
-    game.init();
-    game.run();
+	Game& game = Game::getInstance();
+	game.init();
+	game.run();
 
-    int b{0};
-    b = 99;
-    return 0;
+	return 0;
 }
